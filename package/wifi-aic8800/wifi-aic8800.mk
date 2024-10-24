e1b2d506733b728acbdb3c728d7b248fb31edf64
jim smith <47976944+jimsmt@users.noreply.github.com>
2024-10-25 00:45:07 +0800

    Update wifi-aic8800.mk

--- package/wifi-aic8800/wifi-aic8800.mk
+++ package/wifi-aic8800/wifi-aic8800.mk
@@ -1,5 +1,5 @@
 WIFI_AIC8800_SITE_METHOD = git
-WIFI_AIC8800_SITE = https://github.com/openipc/aic8800
+WIFI_AIC8800_SITE = https://github.com/jimsmt/aic8800
 WIFI_AIC8800_SITE_BRANCH = master
 WIFI_AIC8800_VERSION = $(shell git ls-remote $(WIFI_AIC8800_SITE) $(WIFI_AIC8800_SITE_BRANCH) | head -1 | cut -f1)
 
