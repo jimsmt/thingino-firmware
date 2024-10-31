a758997b1b09df752a739841c918687372471611
jim smith <47976944+jimsmt@users.noreply.github.com>
2024-10-31 20:09:39 +0800

    Update wifi-ssv6x5x.mk

--- package/wifi-ssv6x5x/wifi-ssv6x5x.mk
+++ package/wifi-ssv6x5x/wifi-ssv6x5x.mk
@@ -1,5 +1,6 @@
 WIFI_SSV6X5X_SITE_METHOD = git
 WIFI_SSV6X5X_SITE = https://github.com/jimsmt/ssv6x5x-openipc
+CAMERA_CONFIG_REAL = $(shell realpath $(BR2_EXTERNAL)/configs/$(CAMERA_CONFIG))
 $(info $shell(cat $(CAMERA_CONFIG_REAL)))
 WIFI_SSV6X5X_SITE_BRANCH = pid_$(shell cat $(CAMERA_CONFIG_REAL) | grep SSV6X5X_PID|cut -d "=" -f2)
 $(info SSV6X5X branch is $(WIFI_SSV6X5X_SITE_BRANCH))
